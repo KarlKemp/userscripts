f818b593d234f86cafaa6b9be378b4f33ee53f4c
Matthias Winkelmann <m@matthi.coffee>
2019-11-17 09:08:45 +0100

    fix

--- previews.user.js
+++ previews.user.js
@@ -1,13 +1,16 @@
 // ==UserScript==
-// @name        New script - imagefap.com
+// @name        Larger Previews imagefap.com
 // @namespace   Violentmonkey Scripts
-// @match       https://www.imagefap.com/pictures/4694747/Amateur-Wives-Girlfriends-94-Part-1
+// @match       https://www.imagefap.com/*
 // @grant       none
-// @version     1.0
+// @version     1.2
 // @author      Karl Kemp
+// @homepage    https://chilling.guru
+// @updateURL   https://github.com/KarlKemp/userscripts/raw/master/previews.user.js
 // @description 11/17/2019, 7:54:58 AM
+// @esversion
 // ==/UserScript==
-
+/* jshint esversion: 6 */
 Array.from(document.getElementsByClassName("gal_mini")).forEach(im => {
    im.srcset = im.src.replace("mini", "thumb") + " 20w";
    im.style = "width:144px; min-height:96px;";
